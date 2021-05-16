8554a051a68f0c7b01cc5eaaf2f0e4541a6b34bc
Ric Cox <rpcox@cox.net>
2021-05-16 21:10:25 +0000

    210516a

--- main.go
+++ main.go
@@ -9,7 +9,13 @@ import (
 	"strings"
 )
 
-type MetricLine struct {   // Field names from 'syslog-ng-ctl stats' call
+type MetricType struct {
+  counter string
+  gauge   string
+}
+
+
+type SNG_MetricLine struct {   // Field names from 'syslog-ng-ctl stats' call
 	objectType string  // SourceName
 	id         string  // SourceId
 	instance   string  // SourceInstance
@@ -18,7 +24,19 @@ type MetricLine struct {   // Field names from 'syslog-ng-ctl stats' call
 	value      float64 // Number
 }
 
-func makeMetricName(MetricLine m) label string {
+func TypeLine (metricName string, metricType string) {
+	s:= []string{"# TYPE ", metricName, " ", metricType}
+	return strings.Join(s,"_")
+}
+
+func MetricLine(metricName string, SNG_MetricLine ml) {
+	s:= []string{metricName, "{id\"", ml.id, "\",item=\"", ml.item, "\",state=\"", ml.state, "\",type=\"", ml.statType, "\"} "}
+}
+
+func MetricName(MetricLine m) string {
+	s:= []string{"sng", m.objectType}
+	s = strings.Join(s, "_")
+	return strings.ReplaceAll(s, ".", "_")
 }
 
 
@@ -37,26 +55,28 @@ func parseLine(line string) (MetricLine, error) {
 	return MetricLine{chunk[0], chunk[1], chunk[2], chunk[3], chunk[4], num}, nil
 }
 
-func getStats() {
+func GetSNGStats() {
+	mt := MetricType("counter", "gauge")
 	c, err := net.Dial("unix", "/var/lib/syslog-ng/syslog-ng.ctl")
 
 	if err != nil {
-		log.Print("syslog-ng.ctl connect fail:", err)
+		log.Print("syslog-ng.ctl connect error: ", err)
 		return
 	}
 
 	defer c.Close()
-
 	_, err = c.Write([]byte("STATS\n"))
+
 	if err != nil {
-		log.Print("syslog-ng.ctl write error:", err)
+		log.Print("syslog-ng.ctl write error: ", err)
 		return
 	}
 
 	buf := bufio.NewReader(c)
-
 	_, err = buf.ReadString('\n')
+	
 	if err != nil {
+		log.Print("syslog-ng.ctl read error: ", err)
 		return
 	}
 
@@ -68,16 +88,37 @@ func getStats() {
 			break
 		}
 
-		data, err := parseLine(line)
-		if err != nil {
+		if line[4] = 'o' || line[4] = 'd' { // don't want orphans or dynamics
 			continue
 		}
 
-		fmt.Println(data)
+		sngData, err := parseLine(line)
+		if err != nil {
+			fmt.Println("parse error: ", err)
+			continue
+		}
+
+		name := generateMetricName(line)
+
+		switch sngData.objectType[0:4} {
+			case "src."
+			        switch sngData.statType {
+				case "processed"
+				        fmt.Println(TypeLine(name, mt.gauge)
+				case "stamp":
+				        fmt.Println(TypeLine(name, mt.counter)
+				}
+			case "dst.":
+			case "filt":
+			//default:
+		}
+
+		fmt.Println(MetricLine(name, sngData)))
 	}
 
 }
 
+
 func main() {
-	getStats()
+	GetSNGStats()
 }
